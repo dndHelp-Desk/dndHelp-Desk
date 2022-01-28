0210c96ae8c4c2728999462632c68ab1a52a70c1
Sands <82897602+Sands-45@users.noreply.github.com>
2022-01-28 23:52:07 +0200

    Error clearing

--- src/components/Tickets/TicketsList.js
+++ src/components/Tickets/TicketsList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { BsEnvelope, BsChevronDown } from "react-icons/bs";
+import { BsEnvelope } from "react-icons/bs";
 import placeHolderImg from "./images/email-open.png";
 import {
   changePriority,
