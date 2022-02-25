19a488572b8ea740d066276664f139a53e804942
Sands <82897602+Sands-45@users.noreply.github.com>
2022-02-25 16:06:13 +0200

    Firebase Auth

--- src/components/authentication/Firebase.js
+++ src/components/authentication/Firebase.js
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -11,19 +10,6 @@ import {
 } from "firebase/auth";
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 
-//Config Firebase ==================================
-const firebaseConfig = {
-  apiKey: "AIzaSyA62ZCv7_0Ktz4y-gU2EvHIpKTn3WFXM3U",
-  authDomain: "dial-n-dine-help-desk.firebaseapp.com",
-  projectId: "dial-n-dine-help-desk",
-  storageBucket: "dial-n-dine-help-desk.appspot.com",
-  messagingSenderId: "143621322408",
-  appId: "1:143621322408:web:14f3e35a0529b1f5dc749d",
-  measurementId: "G-GZCS9SQW3Z",
-};
-
-// Initialize Firebase for auth======================
-initializeApp(firebaseConfig);
 const auth = getAuth();
 const storage = getStorage();
 
--- src/components/authentication/LogIn.js
+++ src/components/authentication/LogIn.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 import {
   getAuth,
   onAuthStateChanged,
@@ -13,6 +15,20 @@ import Background from "./images/welcome.jpg";
 import { isAuthenticated } from "../../store/UserSlice";
 import Alert from "../Others/Alert";
 import { updateAlert } from "../../store/NotificationsSlice";
+
+//Config Firebase ==================================
+const firebaseConfig = {
+  apiKey: "AIzaSyA62ZCv7_0Ktz4y-gU2EvHIpKTn3WFXM3U",
+  authDomain: "dial-n-dine-help-desk.firebaseapp.com",
+  projectId: "dial-n-dine-help-desk",
+  storageBucket: "dial-n-dine-help-desk.appspot.com",
+  messagingSenderId: "143621322408",
+  appId: "1:143621322408:web:14f3e35a0529b1f5dc749d",
+  measurementId: "G-GZCS9SQW3Z",
+};
+
+// Initialize Firebase for auth======================
+initializeApp(firebaseConfig);
 
 //Initialize Services ======
 const auth = getAuth();
