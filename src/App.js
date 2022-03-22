abd2913332018ef77fbaa08f4f280b853ef7736a
Sands <82897602+Sands-45@users.noreply.github.com>
2022-03-22 19:38:57 +0200

    Added Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./components/Landing Page/Landing";
 import LogIn from "./components/authentication/LogIn";
@@ -7,42 +7,53 @@ import ReportsComponent from "./components/Reports/ReportComponent";
 import TicketsComponent from "./components/Tickets/TicketsComponent";
 import ContactsComponent from "./components/Contacts/ContactsComponent";
 import SettingsComponent from "./components/Settings/SettingsComponents";
-import SupportHome from "./components/ClientSupport/SupportHome";
-import NewTicket from "./components/ClientSupport/NewTicket";
-import KnowledgeBase from "./components/ClientSupport/KnowledgeBase";
-import Account from "./components/Settings/Account";
-import Team from "./components/Settings/Team";
-import SupportOperations from "./components/Settings/Support Operations/SupportOperations";
-import AutomatedReports from "./components/Settings/AutomatedReports";
-import Solutions from "./components/Settings/Solutions";
+const SupportHome = lazy(() => import("./components/ClientSupport/SupportHome"));
+const NewTicket = lazy(() => import("./components/ClientSupport/NewTicket"));
+const KnowledgeBase = lazy(() => import("./components/ClientSupport/KnowledgeBase"));
+const Account = lazy(() => import("./components/Settings/Account"));
+const Team = lazy(() => import("./components/Settings/Team"));
+const SupportOperations = lazy(() => import("./components/Settings/Support Operations/SupportOperations"));
+const AutomatedReports = lazy(() => import("./components/Settings/AutomatedReports"));
+const Solutions = lazy(() => import("./components/Settings/Solutions"));
 
 const App = () => {
   return (
     <div className="overflow-hidden">
       <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Landing />} />
-          <Route exact path="/logIn" element={<LogIn />} />
-          <Route exact path="/support" element={<SupportHome />}>
-            <Route path="new-ticket" element={<NewTicket />} />
-            <Route path="knowledge_base" element={<KnowledgeBase />} />
-          </Route>
-          <Route path="/app" element={<MainComponent />}>
-            <Route path="tickets" element={<TicketsComponent />} />
-            <Route path="reports" element={<ReportsComponent />} />
-            <Route path="contacts" element={<ContactsComponent />} />
-            <Route path="settings" element={<SettingsComponent />}>
-              <Route path="account" element={<Account />} />
-              <Route path="team" element={<Team />} />
-              <Route
-                path="support-operations"
-                element={<SupportOperations />}
-              />
-              <Route path="automated-reports" element={<AutomatedReports />} />
-              <Route path="solutions" element={<Solutions />} />
+        <Suspense
+          fallback={
+            <div
+              className="h-screen w-screen bg-slate-900 flex items-center justify-center"
+            ><h1 className="text-slate-300 text-3xl font-bold italic capitalize">Loading ...</h1></div>
+          }
+        >
+          <Routes>
+            <Route exact path="/" element={<Landing />} />
+            <Route exact path="/logIn" element={<LogIn />} />
+            <Route exact path="/support" element={<SupportHome />}>
+              <Route path="new-ticket" element={<NewTicket />} />
+              <Route path="knowledge_base" element={<KnowledgeBase />} />
             </Route>
-          </Route>
-        </Routes>
+            <Route path="/app" element={<MainComponent />}>
+              <Route path="tickets" element={<TicketsComponent />} />
+              <Route path="reports" element={<ReportsComponent />} />
+              <Route path="contacts" element={<ContactsComponent />} />
+              <Route path="settings" element={<SettingsComponent />}>
+                <Route path="account" element={<Account />} />
+                <Route path="team" element={<Team />} />
+                <Route
+                  path="support-operations"
+                  element={<SupportOperations />}
+                />
+                <Route
+                  path="automated-reports"
+                  element={<AutomatedReports />}
+                />
+                <Route path="solutions" element={<Solutions />} />
+              </Route>
+            </Route>
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </div>
   );
