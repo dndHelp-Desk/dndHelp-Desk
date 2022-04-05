2ad4f75f6ab0bfb241830c6765158ffee0bfaf8e
Sands <82897602+Sands-45@users.noreply.github.com>
2022-04-05 07:00:49 +0200

    Build Update

--- src/components/MainComponent/SettingsTooltip.js
+++ src/components/MainComponent/SettingsTooltip.js
@@ -20,7 +20,7 @@ const SettingsTooltip = () => {
         role="tooltip"
         className="bg-transparent p-1 pt-6 absolute right-[-2.3rem] top-[2.2rem] hidden group-hover:flex flex-col"
       >
-        <div className="min-h-[10rem] w-[13rem] z-[999] dark:bg-slate-700 bg-white border dark:border-slate-700 border-slate-300 pt-3 no-scrollbar no-scrollbar::-webkit-scrollbar scroll-snap shadow-2xl rounded-lg p-2 relative  after:content-[''] after:absolute after:right-[2.5rem] after:top-[-0.8rem] after:h-6 after:w-6 after:rotate-45 after:bg-inherit after:border-t  after:border-l after:border-inherit flex flex-col items-center">
+        <div className="min-h-[10rem] w-[13rem] z-[999] dark:bg-slate-800 bg-white border dark:border-slate-700 border-slate-300 pt-3 no-scrollbar no-scrollbar::-webkit-scrollbar scroll-snap shadow-2xl drop-shadow-2xl rounded-lg p-2 relative  after:content-[''] after:absolute after:right-[2.5rem] after:top-[-0.8rem] after:h-6 after:w-6 after:rotate-45 after:bg-inherit after:border-t  after:border-l after:border-inherit flex flex-col items-center">
           <h2 className="dark:text-slate-300 text-slate-800 text-xl font-bold text-left w-full px-2">
             Settings
           </h2>
