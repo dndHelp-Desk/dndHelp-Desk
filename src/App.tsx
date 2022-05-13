fad8f72db2cf594520dc34ca3e2a8e42a1da759b
Sands <82897602+Sands-45@users.noreply.github.com>
2022-05-13 19:56:51 +0200

    Redirects Folders Changed to redirects because it confllicts with CI/CD config files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { FC, Suspense, lazy } from "react";
-import Redirect from "./Components/Auth/Redirects";
+import Redirect from "./Components/Auth/LogRedirect";
 import SetUpRedirect from "./Components/Landing Page/SetUpRedirect";
 import ReportsComponent from "./Components/Reports/ReportComponent";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
